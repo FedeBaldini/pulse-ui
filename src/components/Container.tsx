de79bc79a0a45f1c2a9bfba713b5d5eb2c9c76fb
Federico Baldini <federico@condense.tech>
2024-01-17 12:21:03 +0100

    update comment

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,6 +7,8 @@ export function Container({
   className
 }: WithChildren & WithOptionalClassname) {
   return (
-    <div className={classNames("container mx-auto", className)}>{children}</div>
+    <div className={classNames("container mx-auto px-4 md:p-0", className)}>
+      {children}
+    </div>
   );
 }

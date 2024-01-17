50ecedec09eeeca40e6847b03b640a7c3fa6f55c
Federico Baldini <federico@condense.tech>
2024-01-17 11:19:57 +0100

    adjust modal

--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -1,18 +1,34 @@
-import { Button } from "@fbaldini/pulse-ui";
+import { Button, Modal, Title, useModal } from "@fbaldini/pulse-ui";
 
 export default function Home() {
+  const { close, isOpen, open } = useModal();
   return (
     <main>
-      <Button
-        style="primary"
-        small
-        outlined
-        onClick={() => {
-          alert("Clicked!");
-        }}
-      >
-        Oddio
+      <Button style="primary" small outlined onClick={open}>
+        Open modal
       </Button>
+      <Modal isOpen={isOpen} closeOnOutsideClick>
+        <Modal.Header onClose={close}>
+          <Title as="h3">Lorem ipsum dolor sit amet</Title>
+        </Modal.Header>
+        <Modal.Content>
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in
+            euismod nulla. Nullam sagittis lectus ut velit blandit iaculis.
+            Etiam posuere, nunc ac iaculis fringilla, tellus sem tincidunt
+            ipsum, ac mattis lacus velit a tellus.
+          </p>
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Donec viverra, odio a bibendum imperdiet, turpis ipsum vestibulum
+            ligula, at suscipit diam ipsum id nunc. Integer accumsan, libero id
+            mollis imperdiet, metus velit finibus enim, eget suscipit lectus
+            nisi vel nunc.
+          </p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button children="Close" outlined onClick={close} />
+        </Modal.Actions>
+      </Modal>
     </main>
   );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -28,8 +28,8 @@ export function Modal({
       className={classNames(
         { hidden: !isOpen },
         "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50",
-        "flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full",
-        "bg-neutral-extra-light bg-opacity-75"
+        "flex justify-center items-center w-full h-full max-h-full md:inset-0",
+        "bg-[rgb(236,240,241,.8)]"
       )}
       data-testid="ModalBackground"
       onClick={closeOnOutsideClick ? onClose : undefined}

c3529c72cba5dc909dbb625c8faf6f8baf4a9d7a
Federico Baldini <federico@condense.tech>
2024-01-17 11:01:31 +0100

    export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,12 @@
+export * from "./Alert";
+export * from "./Avatar";
 export * from "./Button";
+export * from "./Comment";
 export * from "./Container";
+export * from "./Loader";
+export * from "./Modal";
+export * from "./Segment";
+export * from "./Tag";
+export * from "./Title";
+
 export * from "./forms";

e749448fdf068a0ac014cfca03d0bb1666d4ecae
Federico Baldini <federico@condense.tech>
2024-01-18 09:22:12 +0100

    update tests

--- src/components/__tests__/Section.spec.tsx
+++ src/components/__tests__/Section.spec.tsx
@@ -10,16 +10,16 @@ describe("components/Section", () => {
 
   it("renders as padded by default", () => {
     render(<Section children="Section" />);
-    expect(screen.getByText("Section")).toHaveClass("p-4 md:p-6");
+    expect(screen.getByText("Section")).toHaveClass("my-4 md:my-6");
   });
 
   it("renders as padded when explicited", () => {
     const { unmount } = render(<Section children="Section" padded />);
-    expect(screen.getByText("Section")).toHaveClass("p-4 md:p-6");
+    expect(screen.getByText("Section")).toHaveClass("my-4 md:my-6");
 
     unmount();
     render(<Section children="Section" padded={false} />);
 
-    expect(screen.getByText("Section")).not.toHaveClass("p-4 md:p-6");
+    expect(screen.getByText("Section")).not.toHaveClass("my-4 md:my-6");
   });
 });

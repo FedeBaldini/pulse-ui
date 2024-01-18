93f3d04d4fc3a9138b10fb6ff8692bb64f5b8675
Federico Baldini <federico@condense.tech>
2024-01-18 08:05:47 +0100

    add breadcrumb

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,52 @@
+import classNames from "classnames";
+
+import { WithChildren, WithOptionalClassname } from "../types";
+
+export interface BreadcrumbProps extends WithChildren, WithOptionalClassname {
+  withBackground?: boolean;
+}
+
+export function Breadcrumb({
+  children,
+  className,
+  withBackground
+}: BreadcrumbProps) {
+  return (
+    <nav
+      className={classNames(
+        "flex px-5 py-3 text-gray-700 rounded-default",
+        { "border border-gray-200 bg-gray-50": withBackground },
+        className
+      )}
+      aria-label="Breadcrumb"
+    >
+      <ol className="inline-flex items-center space-x-1 md:space-x-2">
+        {Array.isArray(children)
+          ? children.map((child, index) => (
+              <li key={index} className="inline-flex items-center">
+                {child}
+              </li>
+            ))
+          : null}
+      </ol>
+    </nav>
+  );
+}
+
+function BreadcrumbItem({
+  children,
+  className
+}: WithChildren & WithOptionalClassname) {
+  return (
+    <div
+      className={classNames(
+        "flex items-center ms-1 text-sm font-medium md:ms-2",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+Breadcrumb.Item = BreadcrumbItem;

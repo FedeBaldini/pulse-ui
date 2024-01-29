7d6c756a6522fdfd04b92806ebc2a58559ad63e8
Federico Baldini <federico@condense.tech>
2024-01-29 08:39:05 +0100

    upgrade version

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -30,7 +30,7 @@ export function Dropdown({ label, children, ...props }: DropdownProps) {
         </div>
       </Button>
       {isOpen ? (
-        <div className="absolute z-10 bg-white rounded-generic shadow w-44">
+        <div className="absolute z-10 bg-white rounded-generic shadow w-full min-w-44 top-[calc(100%_+_4px)]">
           <ul className="py-2 text-sm">{items}</ul>
         </div>
       ) : null}
